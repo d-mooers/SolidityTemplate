4b12ef65e1085f97d5ad1974fbfcd4083977169a
Rahu <rahunandan@gmail.com>
2022-12-06 09:53:01 +0400

    feat: add minima_contracts

---
+++ src/minima_contracts/interfaces/stableswap/ISwap.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.8;
+
+interface ISwap {
+	function paused() external view returns (bool);
+	function getToken(uint8 index) external view returns (address);
+	function getBalances() external view returns (uint256[] memory);
+	function getSwapFee() external view returns (uint256);
+	function getAPrecise() external view returns (uint256);
+	function getLpToken() external view returns (address);
+
+	function swap(
+		uint8 tokenIndexFrom,
+		uint8 tokenIndexTo,
+		uint256 dx,
+		uint256 minDy,
+		uint256 deadline
+	) external returns (uint256);
+
+	function calculateSwap(
+		uint8 tokenIndexFrom,
+		uint8 tokenIndexTo,
+		uint256 dx
+	) external view returns (uint256);
+
+	function addLiquidity(
+        uint256[] calldata amounts,
+        uint256 minToMint,
+        uint256 deadline
+    )
+        external
+        returns (uint256);
+
+	function removeLiquidity(
+        uint256 amount,
+        uint256[] calldata minAmounts,
+        uint256 deadline
+    ) external returns (uint256[] memory);
+}
---
+++ src/minima_contracts/utils/Math.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.6.8;
+
+library Math {
+    /**
+     * @dev Returns the largest of two numbers of 256 bits.
+     */
+    function max(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a >= b ? a : b;
+    }
+
+    /**
+     * @dev Returns the smallest of two numbers of 256 bits.
+     */
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+}
